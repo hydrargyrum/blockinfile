ea292dd6fe28cdf06cf035a85f9dbcd34f9fb84c
nobody <nobody@localhost>
2023-02-02 18:23:04 +0100

    add a builder script

---
+++ builder/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -eu
+
+cd "$(dirname "$0")"
+FILE=blockinfile.py
+
+echo "= Building environment ============================="
+
+mkdir -p env
+python3 -m venv env
+. ./env/bin/activate
+
+python3 -m pip install -r requirements.txt
+
+echo "= Patching file ===================================="
+
+cp env/lib/python3*/site-packages/ansible/modules/"$FILE" .
+
+# __future__ imports are useless for us, and they cry if not imported early so
+# delete them
+sed -i '/^from ansible/d ; /^from __future__ import/d' "$FILE"
+
+# it seems sed's "r" command forcefully prepends the pattern space
+# so it's hard to find a place where to inject our code…
+# inject the shebang here too
+sed -i '1 {
+	s@.*@#!/usr/bin/env python3@
+	r insert.txt
+}' "$FILE"
+chmod +x "$FILE"
+
+echo "= Done! ============================================"
