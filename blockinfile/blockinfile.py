b5fbe9382a3cb4278df7ecec65560c9420b333cd
nobody <nobody@localhost>
2023-08-01 13:12:36 +0200

    bump version

--- blockinfile/blockinfile.py
+++ blockinfile/blockinfile.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-3.0-only
 # this file has been generated from ansible sources + injected code
 # manual edits might be lost
 # license: GPLv3+, same as ansible
